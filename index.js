82ebbcc6e0b5b5843d6804648de91ce72514f36f
Matt Kim <kimm5294@gmail.com>
2017-06-18 15:30:31 -0400

    add sentence to search results page that describes weather at current time

--- index.js
+++ index.js
@@ -16,13 +16,12 @@ app.get("/", (req, res)=>{
 app.get("/search", (req, res)=>{
   var url = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22" + req.query.location + "%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys"
 
-  console.log(req.query.location)
 
   request(url, (error, response, body)=> {
     if (!error && response.statusCode == 200) {
       var data = JSON.parse(body);
       var weather = data["query"]["results"]["channel"]
-      console.log(data["query"]["results"]);
+      console.log(data["query"]["results"]["channel"]["item"]["condition"]);
       res.render("search", {data: weather})
     } else {
       res.send("doesn't work")
