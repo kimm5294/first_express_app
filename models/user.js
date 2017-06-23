d5749ee12d905e41652218bab5502aadf184964b
Matt Kim <kimm5294@gmail.com>
2017-06-23 12:06:03 -0400

    fix association typo

--- models/user.js
+++ models/user.js
@@ -1,9 +1,12 @@
 var mongoose = require("mongoose");
+var passportLocalMongoose = require("passport-local-mongoose");
 
 var userSchema = new mongoose.Schema({
   username: String,
   password: String,
-  locations: [{type: Schema.Types.ObjectId, ref: "Location"}]
+  locations: [{type: mongoose.Schema.Types.ObjectId, ref: "Location"}]
 });
+
+userSchema.plugin(passportLocalMongoose);
 
 module.exports = mongoose.model("User", userSchema);
