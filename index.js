51eb3b5db15e3b348272b3d9f964ebf48a61475a
Matt Kim <kimm5294@gmail.com>
2017-06-24 13:03:22 -0400

    create get route for login page

--- index.js
+++ index.js
@@ -33,6 +33,8 @@ app.get("/", (req, res)=>{
   res.render("homepage");
 });
 
+
+//REGISTER ROUTES
 app.get("/register", (req, res)=>{
   res.render("register");
 });
@@ -49,6 +51,11 @@ app.post("/register", (req, res)=>{
     }
   })
 });
+
+//LOGIN ROUTES
+app.get("/login", (req, res)=>{
+  res.render("login");
+})
 
 app.get("/search", (req, res)=>{
   var url = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22" + req.query.location + "%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys"
