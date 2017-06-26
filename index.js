0acedb067924a798df389f5e516e4fc1bcf9767f
Matt Kim <kimm5294@gmail.com>
2017-06-25 20:22:28 -0400

    create route for showing user show page

--- index.js
+++ index.js
@@ -68,7 +68,13 @@ app.post("/login", passport.authenticate("local", {
 app.get("/logout", (req, res)=>{
   req.logout();
   res.redirect("/");
-})
+});
+
+//USER ROUTES
+
+app.get("/:id", (req, res)=>{
+  res.render("users/show");
+});
 
 app.get("/search", (req, res)=>{
   var url = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22" + req.query.location + "%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys"
