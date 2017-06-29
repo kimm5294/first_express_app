379702ccc3737f3880b1dacd486579789bb6ecf7
Matt Kim <kimm5294@gmail.com>
2017-06-29 16:56:29 -0400

    change location field to an array

--- models/user.js
+++ models/user.js
@@ -4,7 +4,7 @@ var passportLocalMongoose = require("passport-local-mongoose");
 var userSchema = new mongoose.Schema({
   username: String,
   password: String,
-  locations: [{type: mongoose.Schema.Types.ObjectId, ref: "Location"}]
+  locations: []
 });
 
 userSchema.plugin(passportLocalMongoose);
