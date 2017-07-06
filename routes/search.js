549e494488dda20a4bb1f26cd7d4520009010f53
Matt Kim <kimm5294@gmail.com>
2017-07-05 21:19:44 -0400

    create route for displaying each locations data

--- routes/search.js
+++ routes/search.js
@@ -20,6 +20,23 @@ router.get("/search", (req, res)=>{
   })
 });
 
+router.get("/display/:place", (req, res)=>{
+
+  var url = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22" + req.params.place + "%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys"
+
+  request(url, (error, response, body)=> {
+    if (!error && response.statusCode == 200 && JSON.parse(body)["query"]["count"]!=0) {
+      var data = JSON.parse(body);
+      var weather = data["query"]["results"]["channel"]
+      var name = weather["location"]["city"] + ", " + weather["location"]["region"] + ", " + weather["location"]["country"]
+      res.render("places/display", {name: name, weather: weather})
+    } else {
+      res.send("doesn't work")
+    }
+  })
+
+})
+
 router.post("/save", (req, res)=>{
   req.user.locations.push(req.body.location);
   req.user.save((err, user)=>{
